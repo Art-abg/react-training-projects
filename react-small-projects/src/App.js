9dfcb2cb3cb38f35bb34c58da1e5e95b07e1036d
Artur Abgaryan <arty219603@gmail.com>
2024-02-19 14:29:25 +0400

    started a react project

---
+++ react-small-projects/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Accordion from "./components/accordion/accordion";
+
+function App() {
+  return (
+    <div className="App">
+      {/* accordian component goes here*/}
+      <Accordion />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-small-projects/src/components/accordion/accordion.jsx
@@ -0,0 +1,11 @@
+//single selection
+//multiple selection
+import data from "./data";
+import React, { useState } from "react";
+import "./accordion.css";
+
+export default function Accordian() {
+  const [selected, setSelected] = useState(null);
+
+  return <div className="wrapper"></div>;
+}
